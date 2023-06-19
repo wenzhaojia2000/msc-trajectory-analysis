828d2f608e79bfa523825eb28a4f5f449ef5b04f
James Jia <32031865+wenzhaojia2000@users.noreply.github.com>
2023-06-19 23:25:23 +0100

    Merge system evolution and potential energy surface tabs

--- source/analyse/gui_files/gui_analysis/analysis_system.py
+++ source/analyse/gui_files/gui_analysis/analysis_system.py
@@ -27,16 +27,16 @@ class AnalysisSystem(QtWidgets.QMainWindow, AnalysisBase):
         Finds objects related to the current tab from the parent AnalysisMain
         instance.
         '''
-        self.analevol_push = self.owner.findChild(QtWidgets.QPushButton, 'analevol_push')
-        self.analevol_box = self.owner.findChild(QtWidgets.QBoxLayout, 'analevol_layout')
-        self.analevol_radio = [self.analevol_box.itemAt(i).widget() \
-                               for i in range(self.analevol_box.count())]
+        self.analsys_push = self.owner.findChild(QtWidgets.QPushButton, 'analsys_push')
+        self.analsys_box = self.owner.findChild(QtWidgets.QBoxLayout, 'analsys_layout')
+        self.analsys_radio = [self.analsys_box.itemAt(i).widget() \
+                               for i in range(self.analsys_box.count())]
 
     def connectObjects(self) -> None:
         '''
         Connects objects so they do stuff when interacted with.
         '''
-        self.analevol_push.clicked.connect(self.continuePushed)
+        self.analsys_push.clicked.connect(self.continuePushed)
 
     @QtCore.pyqtSlot()
     def continuePushed(self) -> None:
@@ -46,14 +46,14 @@ class AnalysisSystem(QtWidgets.QMainWindow, AnalysisBase):
         # working directory
         abspath = self.owner.dir_edit.text()
         # get objectName() of checked radio button (there should only be 1)
-        radio_name = [radio.objectName() for radio in self.analevol_radio
+        radio_name = [radio.objectName() for radio in self.analsys_radio
                       if radio.isChecked()][0]
         match radio_name:
-            case 'analevol_1': # plot 1d density evolution
+            case 'analsys_1': # plot 1d density evolution
                 self.owner.runCmd('showd1d', '-inter', '-i', abspath, input_='1')
-            case 'analevol_2': # plot 2d density evolution
+            case 'analsys_2': # plot 2d density evolution
                 self.owner.runCmd('showsys', '-i', abspath)
-            case 'analevol_3': # plot diabatic state population
+            case 'analsys_3': # plot diabatic state population
                 self.owner.runCmd('plstate', '-i', abspath)
-            case 'analevol_4': # plot potential energy surface
+            case 'analsys_4': # plot potential energy surface
                 self.owner.runCmd('showsys', '-pes', '-i', abspath, input_='1')
